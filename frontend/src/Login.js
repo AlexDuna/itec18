7f1b6893a2d45a027ff487464040607797f5a53e
kickhead13 <ana.alexandru.gabriel@proton.me>
2025-04-05 15:46:10 +0000

    frontend: TESTED
    Signed-off-by: kickhead13 <ana.alexandru.gabriel@proton.me>

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -97,7 +97,7 @@ axios.post('https://onlinedi.vision/api/new_user', payload, config)
           <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)}  placeholder="Username" required/>
           <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" required/>
           <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}  placeholder="Password" required/>
-          <button className="submit" type="submit">Login</button>
+          <button className="submit" onClick={()=>{signup(username,password,email)}}>Sign Up</button>
           
           <button type="button"  value={state} onClick={()=>setState(1)}>Already have an account?</button>
         </div>
